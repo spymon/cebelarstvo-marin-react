964dbbaaa91b8c25b41b3ca488da795df5ba2d36
Simon Žižek <spymon12@gmail.com>
2021-05-25 10:05:56 +0200

    add navbar and navbar styled components, add favicon and Prompt font from google font

--- frontend/src/components/Navbar/NavbarElements.js
+++ frontend/src/components/Navbar/NavbarElements.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components'
 import { Link as LinkR } from 'react-router-dom'
-// import {Link as linkS} from 'react-scroll'
+import { Link as LinkS } from 'react-scroll'
 
 export const Nav = styled.nav`
-  background: #000;
-  height: 80px;
-  /* margin-top: -80px; */
+  background: #fff;
+  height: 100px;
+  /* margin-top: -100px; */
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 1rem;
+  border-bottom: 3px solid #f7d433;
+  box-shadow: 0 3px 10px rgba(51, 51, 51, 0.2);
   position: sticky;
   top: 0;
   z-index: 10;
@@ -29,14 +31,103 @@ export const NavbarContainer = styled.div`
   max-width: 1100px;
 `
 
-export const NavLogo = styled(LinkR)`
-  color: #fff;
+// react router link
+export const NavLogoWrapper = styled(LinkR)`
+  position: relative;
+  top: -8px;
   justify-self: flex-start;
   cursor: pointer;
   font-size: 1.1rem;
   display: flex;
+  justify-content: center;
   align-items: center;
-  margin-left: 24px;
-  font-weight: bold;
   text-decoration: none;
+  height: 100px;
+`
+
+export const NavLogoImg = styled.img`
+  max-width: 100%;
+  padding: 0;
+  vertical-align: middle;
+  display: inline-block;
+  height: 87px;
+  user-select: none;
+`
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    color: #35363a;
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  // margin-right: -22px;
+
+  @media Screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+export const NavItem = styled.li`
+  height: 80px;
+  display: flex;
+  align-items: center;
+`
+
+// react scroll link
+export const NavLinks = styled(LinkS)`
+  color: #35363a;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  text-transform: uppercase;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+
+  &:active {
+    border-bottom: 3px solid #35363a;
+  }
+`
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+export const NavBtnLink = styled(LinkR)`
+  border-radius: 3.125em;
+  background: #f7d433;
+  white-space: nowrap;
+  padding: 0.625em 1.375em;
+  color: #35363a;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  text-transform: uppercase;
+  text-decoration: none;
+  outline: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #35363a;
+    color: #f7d433;
+  }
 `
--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,13 +1,47 @@
 import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+import { GoThreeBars } from 'react-icons/go'
+import logoImage from '../../images/CebelarstvoMarin_logotip.jpg'
+import {
+  Nav,
+  NavbarContainer,
+  NavLogoWrapper,
+  NavLogoImg,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements'
 
 export const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to="/">LOGO</NavLogo>
-          <h1>Hey</h1>
+          <NavLogoWrapper to="/">
+            <NavLogoImg src={logoImage} alt="Cebelarstvo marin" />
+          </NavLogoWrapper>
+          <MobileIcon>
+            <GoThreeBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="products">Izdelki</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="gallery">Galerija</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="about">O Meni</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contact">Kontakt</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">prijavi se</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
