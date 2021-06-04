509bc4060a514d9463a7b1af305a600880c6f465
Simon Žižek <spymon12@gmail.com>
2021-06-04 18:12:32 +0200

    Add white-space with padding bottom to the Container component

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -6,7 +6,7 @@ export const Container = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: ${({ center }) => (center ? 'center' : 'flex-start')};
-  padding: 0 30px;
+  padding: 0 30px 4rem;
   min-height: 100vh;
   background-color: ${({ dark }) => (dark ? '#35363a' : '#fff')};
 `
