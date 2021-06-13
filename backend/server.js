dbd82fc281e1f89bd0332620a8b22e4ec74afb02
Simon Žižek <spymon12@gmail.com>
2021-06-13 13:22:58 +0200

    changed folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const mongoose = require('mongoose')
 
 //Connection to the DB
-mongoose.connect(process.env.DB_URI,
-  { 
+mongoose.connect(
+  process.env.DB_URI,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-   },
-   () => console.log('connected to db!')
+    useUnifiedTopology: true,
+  },
+  () => console.log('connected to db!')
 )
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 //Import Routes
 const productRoute = require('./routes/products')
@@ -24,4 +24,6 @@ app.use(bodyParser.json())
 //Route Middleware
 app.use('/products', productRoute)
 
-app.listen(PORT, () => {console.log(`listening on port: ${PORT}`)})
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`)
+})
